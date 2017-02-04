b1403baad4a1d8d853456a066e5eccbe743d84a8
Abishek Bhat <bhat.abishek@gmail.com>
2017-02-04 16:57:33 +0530

    Add get_model.

--- data-driver/analyze.py
+++ data-driver/analyze.py
@@ -2,12 +2,24 @@ import numpy as np
 import xgboost
 from sklearn import model_selection
 from sklearn.metrics import accuracy_score
-from sklearn import cross_validation
+from sklearn import cross_validation, linear_model
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import Imputer
 from math import sqrt
 
 import json
+
+def get_model(name='xgb'):
+    """TODO: Docstring for function.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    if name == 'xgb':
+        return xgboost.XGBRegressor(max_depth=10, n_estimators=500)
+    elif name == 'linearreg':
+        return linear_model.LinearRegression()
 
 data_raw = open('/home/ubuntu/data/sales_data_all_subsegment_rollup.json').read()
 data = json.loads(data_raw)
@@ -48,17 +60,17 @@ test_size = 0.10
 
 # model UNIT SALES information
 X_train, X_test, y_train_unit_sales, y_test_unit_sales = cross_validation.train_test_split(X, Y_unit_sales, test_size=test_size, random_state=seed)
-model_unit_sales = xgboost.XGBRegressor(max_depth=10, n_estimators=500)
+model_unit_sales = get_model('xgb')
 model_unit_sales.fit(X_train, y_train_unit_sales)
 
 # model VOLUME SALES information
 X_train, X_test, y_train_volume_sales, y_test_volume_sales = cross_validation.train_test_split(X, Y_volume_sales, test_size=test_size, random_state=seed)
-model_volume_sales = xgboost.XGBRegressor(max_depth=10, n_estimators=500)
+model_volume_sales = get_model('xgb')
 model_volume_sales.fit(X_train, y_train_volume_sales)
 
 # model VOLUME SHARE information
 X_train, X_test, y_train_volume_share, y_test_volume_share = cross_validation.train_test_split(X, Y_volume_share, test_size=test_size, random_state=seed)
-model_volume_share = xgboost.XGBRegressor(max_depth=10, n_estimators=500)
+model_volume_share = get_model('xgb')
 model_volume_share.fit(X_train, y_train_volume_share)
 
 # make predictions to evalute model on test data
