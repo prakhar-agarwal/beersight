aeb3551588e77cfbb32e0a628b5de580f870df06
Ubuntu <ubuntu@ip-172-31-24-197.ap-south-1.compute.internal>
2017-02-04 13:05:50 +0000

    Fixes to process models by subsegment

--- data-driver/analyze.py
+++ data-driver/analyze.py
@@ -24,55 +24,63 @@ def get_model(name='xgb'):
         return linear_model.LinearRegression()
 
 # load dataset
-(X, Y_unit_sales, Y_volume_sales, Y_volume_share) = read_data('/home/ubuntu/data/sales_data_all_subsegment_rollup.json')
+(subsegments, dates, columns, X_full, Y_unit_sales_full, Y_volume_sales_full, Y_volume_share_full) = read_data('/home/ubuntu/data/sales_data_all_subsegment_rollup.json')
 
 # prepare training/test split
-seed = 7
-test_size = 0.1
-split_point = round(len(X)*(1-test_size))
-X_train = X[:split_point]
-X_test = X[split_point:]
-y_train_unit_sales = Y_unit_sales[:split_point]
-y_test_unit_sales = Y_unit_sales[split_point:]
-y_train_volume_sales = Y_volume_sales[:split_point]
-y_test_volume_sales = Y_volume_sales[split_point:]
-y_train_volume_share = Y_volume_share[:split_point]
-y_test_volume_share = Y_volume_share[split_point:]
-
-# model UNIT SALES information
-model_unit_sales = xgboost.XGBRegressor(max_depth=10, n_estimators=500)
-model_unit_sales = get_model('xgb')
-model_unit_sales.fit(X_train, y_train_unit_sales)
-
-# model VOLUME SALES information
-model_volume_sales = xgboost.XGBRegressor(max_depth=10, n_estimators=500)
-model_volume_sales = get_model('xgb')
-model_volume_sales.fit(X_train, y_train_volume_sales)
-
-# model VOLUME SHARE information
-model_volume_share = xgboost.XGBRegressor(max_depth=10, n_estimators=500)
-model_volume_share = get_model('xgb')
-model_volume_share.fit(X_train, y_train_volume_share)
-
-# make predictions to evalute model on test data
-predictions_unit_sales = model_unit_sales.predict(X_test)
-predictions_volume_sales = model_volume_sales.predict(X_test)
-predictions_volume_share = model_volume_share.predict(X_test)
-
-# calculate RMS / r2 errors
-rms_unit_sales = sqrt(mean_squared_error(y_test_unit_sales[:20], predictions_unit_sales[:20]))
-rms_volume_sales = sqrt(mean_squared_error(y_test_volume_sales[:20], predictions_volume_sales[:20]))
-rms_volume_share = sqrt(mean_squared_error(y_test_volume_share[:20], predictions_volume_share[:20]))
-print('Unit Sales  r_squared', r2_score(y_test_unit_sales, predictions_unit_sales))
-print('Volume Sales  r_squared', r2_score(y_test_volume_sales, predictions_volume_sales))
-print('Volume Shares  r_squared', r2_score(y_test_volume_share, predictions_volume_share))
-
-models = {
-    'unit_sales': model_unit_sales,
-    'unit_sales': model_volume_sales,
-    'unit_sales': model_volume_share
-}
-
+models = {}
+for (subsegment, s) in zip(subsegments, list(range(0, len(subsegments)))):
+    print('---')
+    print('Training for subsegment', subsegment)
+    X = X_full[s]
+    Y_unit_sales = Y_unit_sales_full[s]
+    Y_volume_sales = Y_volume_sales_full[s]
+    Y_volume_share = Y_volume_share_full[s]
+    seed = 7
+    test_size = 0.1
+    split_point = round(len(X)*(1-test_size))
+    X_train = X[:split_point]
+    X_test = X[split_point:]
+    y_train_unit_sales = Y_unit_sales[:split_point]
+    y_test_unit_sales = Y_unit_sales[split_point:]
+    y_train_volume_sales = Y_volume_sales[:split_point]
+    y_test_volume_sales = Y_volume_sales[split_point:]
+    y_train_volume_share = Y_volume_share[:split_point]
+    y_test_volume_share = Y_volume_share[split_point:]
+    
+    # model UNIT SALES information
+    model_unit_sales = xgboost.XGBRegressor(max_depth=10, n_estimators=500)
+    model_unit_sales = get_model('xgb')
+    model_unit_sales.fit(X_train, y_train_unit_sales)
+    
+    # model VOLUME SALES information
+    model_volume_sales = xgboost.XGBRegressor(max_depth=10, n_estimators=500)
+    model_volume_sales = get_model('xgb')
+    model_volume_sales.fit(X_train, y_train_volume_sales)
+    
+    # model VOLUME SHARE information
+    model_volume_share = xgboost.XGBRegressor(max_depth=10, n_estimators=500)
+    model_volume_share = get_model('xgb')
+    model_volume_share.fit(X_train, y_train_volume_share)
+    
+    # make predictions to evalute model on test data
+    predictions_unit_sales = model_unit_sales.predict(X_test)
+    predictions_volume_sales = model_volume_sales.predict(X_test)
+    predictions_volume_share = model_volume_share.predict(X_test)
+    
+    # calculate RMS / r2 errors
+    rms_unit_sales = sqrt(mean_squared_error(y_test_unit_sales[:10], predictions_unit_sales[:10]))/np.mean(y_test_unit_sales)
+    rms_volume_sales = sqrt(mean_squared_error(y_test_volume_sales[:10], predictions_volume_sales[:10]))/np.mean(y_test_volume_sales)
+    rms_volume_share = sqrt(mean_squared_error(y_test_volume_share[:10], predictions_volume_share[:10]))/np.mean(y_test_volume_share)
+    print('Unit Sales  rms', rms_unit_sales)
+    print('Volume Sales  rms', rms_volume_sales)
+    print('Volume Shares rms', rms_volume_share)
+    
+    models = {
+        'unit_sales': model_unit_sales,
+        'volume_sales': model_volume_sales,
+        'volume_share': model_volume_share
+    }
+    
 print('Saving models')
 pickle.dump(models, open('models.dat', 'wb'))
 
--- data-driver/server.py
+++ data-driver/server.py
@@ -9,45 +9,24 @@ import json
 from dateutil import parser
 import datetime
 
-data_raw = open('/home/ubuntu/data/sales_data_all_subsegment_rollup.json').read()
-data = json.loads(data_raw)
+from utils.loader import read_data
 
-# get all the subsegments possible
-subsegments = []
-for row in data:
-    if( row['ab_subsegment_value'] not in subsegments ):
-        subsegments.append(row['ab_subsegment_value'])
+# load dataset
+(subsegments, time_labels, columns, X, Y_unit_sales, Y_volume_sales, Y_volume_share) = read_data('/home/ubuntu/data/sales_data_all_subsegment_rollup.json')
 
-removal = ['ab_subsegment_value', 'date', 'sum_unit_sales', 'sum_volume_sales', 'sum_volume_share_of_category']
+# load the model
+models = pickle.load(open('models.dat', 'rb'))
 
-# get all the feature columns possible
-columns = []
-for key in data[0].keys():
-    if( key not in columns and key not in removal ):
-        columns.append(key)
-
-# prepare the X vector
-X = np.zeros((len(data), len(columns)), dtype=np.float)
-i = 0
-for row in data:
-    j = 0
-    for column in columns:
-        X[i][j] = row[column]
-        j += 1
-    i += 1    
-
-# prepare the Y vectors
-Y_unit_sales = np.array([ row["sum_unit_sales"]  for row in data ])
-Y_volume_sales = np.array([ row["sum_volume_sales"]  for row in data ])
-Y_volume_share = np.array([ row["sum_volume_share_of_category"]  for row in data ])
-
-time_labels = []
+print(models)
+print(time_labels)
+print(columns)
+print(X.shape)
+print(subsegments)
 
 data_series = {}
 
-columns = list(data[0].keys())
-
-for row in data:
+for (timestamp, x) in zip(time_labels, X):
+    
     timestamp = parser.parse(row['date']).strftime('%s')
     if timestamp not in time_labels:
         time_labels.append(timestamp)
@@ -58,26 +37,12 @@ for row in data:
     for c in row.keys():
         data_series[row['subsegment']][c].append(row[c])
 
-# # Input-Output Data Formats
-# X = np.array([ [ 
-#     row["sum_display_count"],
-#     row["sum_display_share"],
-#     row["sum_distribution"],
-#     row["sum_feature_count"],
-#     row["sum_feature_share"],
-#     row["avg_price_per_unit"],
-#     row["average_of_max_temperaturec"],
-# ] for row in data ])
-# Y = np.array([ row["sum_unit_sales"]  for row in data ])
-# 
-# # Load the model
-# model = pickle.load('model.data', 'rb')
-# 
-# # make predictions for test data
-# y_pred = model.predict(X_test)
-# predictions = [round(value) for value in y_pred]
-# print( y_test[0:10] )
-# # print( predictions[0:10] )
+
+# make predictions for test data
+y_pred = model.predict(X_test)
+predictions = [round(value) for value in y_pred]
+print( y_test[0:10] )
+print( predictions[0:10] )
 
 # ---------------------------------------------------
 # Flask app to serve the data, model and predictions
